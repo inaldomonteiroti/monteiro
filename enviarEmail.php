1f50b24fe31e26fd3ad48dfd9881220ce880c3a8
Inaldo Monteiro <inaldomonteiroti@gmail.com>
2023-01-31 11:18:07 -0300

    sucesso

--- enviarEmail.php
+++ enviarEmail.php
@@ -2,11 +2,17 @@
 // Passando os dados obtidos pelo formulário para as variáveis abaixo
 $nomeremetente = $_POST['nome'];
 $emailremetente = trim($_POST['email']);
-$emaildestinatario = 'inaldomonteiroti@gmail.com,';// Digite seu e-mail aqui, lembrando que o e-mail deve estar em seu servidor web
+$emaildestinatario = 'inaldomonteiroti@gmail.com';// Digite seu e-mail aqui, lembrando que o e-mail deve estar em seu servidor web
+$assunto = "Contato Site" ;
 $mensagem = $_POST['message'];
 
 /* Montando a mensagem a ser enviada no corpo do e-mail. */
-$assunto = "Formulário do Site";
+$mensagemHTML = '
+<strong>Formulário de Contato</strong> 
+<p><b>Nome:</b> '.$nomeremetente.' <p>
+<b>E-Mail:</b> '.$emailremetente.' <p>
+<b>Mensagem:</b> '.$mensagem.'</p>
+<hr>';
 
 // O remetente deve ser um e-mail do seu domínio conforme determina a RFC 822.
 // O return-path deve ser ser o mesmo e-mail do remetente.
@@ -16,5 +22,7 @@ $headers .= "From: $emailremetente\r\n";
 // remetente
 $headers .= "Return-Path: $emaildestinatario \r\n";
 // return-path
-$envio = mail($emaildestinatario, $assunto, $headers, $mensagem);
+$envio = mail($emaildestinatario, $assunto, $mensagemHTML, $headers);
+if($envio)
+echo "<script>location.href='sucesso.html'</script>";// Página que será redirecionada
 ?>
